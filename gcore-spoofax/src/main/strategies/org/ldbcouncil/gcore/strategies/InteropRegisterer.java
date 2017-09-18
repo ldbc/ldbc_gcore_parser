32a66fabc1a5fb771738ca0ad2caad5521ea7442
Hannes Voigt <hannes.voigt@gmail.com>
2017-09-18 12:41:51 +0200

    initial draft of gcore grammar

---
+++ gcore-spoofax/src/main/strategies/org/ldbcouncil/gcore/strategies/InteropRegisterer.java
@@ -0,0 +1,10 @@
+package org.ldbcouncil.gcore.strategies;
+
+import org.strategoxt.lang.JavaInteropRegisterer;
+import org.strategoxt.lang.Strategy;
+
+public class InteropRegisterer extends JavaInteropRegisterer {
+    public InteropRegisterer() {
+        super(new Strategy[] { });
+    }
+}
---
+++ gcore-spoofax/src/main/strategies/org/ldbcouncil/gcore/strategies/Main.java
@@ -0,0 +1,9 @@
+package org.ldbcouncil.gcore.strategies;
+
+import org.strategoxt.lang.Context;
+
+public class Main {    
+    public static void init(Context context) {
+        
+    }
+}
